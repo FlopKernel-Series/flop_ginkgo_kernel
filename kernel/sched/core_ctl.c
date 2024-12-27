f6da1a6cbb33274fb1b7a22ce6ac384a99944c27
Nahuel Gómez <nahuelgomez329@gmail.com>
2024-12-27 15:02:05 -0300

    Revert "sched: core_ctl: stop userspace from messing it with shitty adjustments"
    Unnecessary right now, also causes these warnings:
    
    ../kernel/sched/core_ctl.c:471:25: warning: unused variable 'ktype_core_ctl' [-Wunused-variable]
      471 | static struct kobj_type ktype_core_ctl = {
          |                         ^~~~~~~~~~~~~~
    ../kernel/sched/core_ctl.c:1177:18: warning: unused function 'try_core_ctl' [-Wunused-function]
     1177 | static int __ref try_core_ctl(void *data)
          |                  ^~~~~~~~~~~~
    ../kernel/sched/core_ctl.c:1253:12: warning: unused function 'core_ctl_isolation_online_cpu' [-Wunused-function]
     1253 | static int core_ctl_isolation_online_cpu(unsigned int cpu)
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../kernel/sched/core_ctl.c:1258:12: warning: unused function 'core_ctl_isolation_dead_cpu' [-Wunused-function]
     1258 | static int core_ctl_isolation_dead_cpu(unsigned int cpu)
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~
    4 warnings generated.
    
    This reverts commit a933df4d7ee13d9c47e7f6803b1fc842bfcc76da.

--- kernel/sched/core_ctl.c
+++ kernel/sched/core_ctl.c
@@ -1262,7 +1262,6 @@ static int core_ctl_isolation_dead_cpu(unsigned int cpu)
 
 /* ============================ init code ============================== */
 
-#if 0
 static struct cluster_data *find_cluster_by_first_cpu(unsigned int first_cpu)
 {
 	unsigned int i;
@@ -1365,4 +1364,3 @@ static int __init core_ctl_init(void)
 }
 
 late_initcall(core_ctl_init);
-#endif
